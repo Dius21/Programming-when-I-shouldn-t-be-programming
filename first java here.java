94c66c68903efcab4f906e61454ed66233dd6c05
Dius21 <virajdhara21@gmail.com>
2023-01-16 10:08:40 +0530

    java test program success!

---
+++ first java here.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class javareturns
+{
+    public static void main(String[] hmm)
+    {
+        System.out.println("How bout that");
+        System.out.println("i am able to use java in vs code");
+    }
+}
