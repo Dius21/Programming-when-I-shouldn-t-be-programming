52d4408afabf3cfe53c9d47bf599192c4e7ec7ed
Dius21 <virajdhara21@gmail.com>
2023-02-08 10:29:19 +0530

    dk y but i made an auto clicker

---
+++ autoclicker.py
@@ -0,0 +1,15 @@
+#brooo this is so slow
+
+
+import pyautogui as pt
+import time
+
+print("BE WARNED!! THIS PROGRAM DOES NOT HAVE ANY END SEQUENCE, IT JUST KEEPS CLICKING. Press CTRL+C TP EXIT RIGHT NOW OR LATER.")
+print("Why are you even using this it's like the slowest method ever")
+delay = input("Enter time delay:")
+time.sleep(int(delay))
+i=0
+
+while i>-1:
+    pt.leftClick()
+
