edc516ff3322998b31fa04fa3c62d2429cf55b0c
Dius21 <virajdhara21@gmail.com>
2023-04-24 15:46:44 +0530

    major cleanup and rearrangement of all files

---
+++ Experimenting/howfastispythonexecuter.py
@@ -0,0 +1,5 @@
+import time
+i=0
+while i<100400:
+    print("Met bob hedge hog  "+str(i+1)+"  "+str(time.process_time()))
+    i+=1
--- howfastispythonexecuter.py
+++
@@ -1,5 +0,0 @@
-import time
-i=0
-while i<10000000000:
-    print("Met bob hedge hog  "+str(i)+"  "+str(time.process_time()))
-    i+=1
