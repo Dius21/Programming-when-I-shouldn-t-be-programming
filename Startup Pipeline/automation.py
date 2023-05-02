ce16f9aab4ebfcba9f8999057110d2a33096769f
Dius21 <virajdhara21@gmail.com>
2023-05-02 13:10:47 +0530

    trying to import module from different a directory

--- Startup Pipeline/automation.py
+++ Startup Pipeline/automation.py
@@ -1,1 +1,5 @@
-HMMM.py
+import importlib
+calculator_gui_importable=  importlib.import_module('C:\Users\\viraj\Programming when I shouldn\'t be Programming\calculator_gui_importable.py')
+import HMMM
+ 
+calculator_gui_importable.GUI()
